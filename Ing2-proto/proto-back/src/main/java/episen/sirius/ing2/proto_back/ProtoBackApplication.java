99ae2dbf63f6711ffdf74d62d01861080953ccdd
Amiraz014 <amiradjaz@hotmail.com>
2024-11-08 14:40:03 +0100

    c1

---
+++ Ing2-proto/proto-back/src/main/java/episen/sirius/ing2/proto_back/ProtoBackApplication.java
@@ -0,0 +1,13 @@
+package episen.sirius.ing2.proto_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoBackApplication.class, args);
+	}
+
+}
---
+++ Ing2-proto/proto-back/src/test/java/episen/sirius/ing2/proto_back/ProtoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package episen.sirius.ing2.proto_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
