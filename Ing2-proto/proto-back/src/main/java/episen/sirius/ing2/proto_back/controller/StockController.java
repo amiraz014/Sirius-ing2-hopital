9967c2dd9c51385b3f1e8a792c7506d0cde92b53
hamzaelm02 <elmoukadem62@gmail.com>
2025-01-24 02:13:53 +0100

    Stock algorithm V17

--- Ing2-proto/proto-back/src/main/java/episen/sirius/ing2/proto_back/controller/StockController.java
+++ Ing2-proto/proto-back/src/main/java/episen/sirius/ing2/proto_back/controller/StockController.java
@@ -4,6 +4,7 @@ import episen.sirius.ing2.proto_back.service.StockService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/stock")
