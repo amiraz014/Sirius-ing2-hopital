7514abb36830e7b42ba4100c157916aff0da89c5
Amiraz014 <amiradjaz@hotmail.com>
2025-02-14 18:16:25 +0100

    changing in client and server name

--- Ing2-proto/proto-back/src/main/java/episen/sirius/ing2/proto_back/repository/MedicamentRepo.java
+++
@@ -1,9 +0,0 @@
-package episen.sirius.ing2.proto_back.repository;
-
-import episen.sirius.ing2.proto_back.model.Medicament;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicamentRepo extends JpaRepository<Medicament, Long> {
-}
