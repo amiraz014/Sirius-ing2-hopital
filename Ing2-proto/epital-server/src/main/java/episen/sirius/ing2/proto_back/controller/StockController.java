5c6c1af643bd5fce8cf28631a402841a43588059
hamzaelm02 <elmoukadem62@gmail.com>
2025-03-04 00:08:50 +0100

    Algo V23

--- Ing2-proto/epital-server/src/main/java/episen/sirius/ing2/proto_back/controller/StockController.java
+++ Ing2-proto/epital-server/src/main/java/episen/sirius/ing2/proto_back/controller/StockController.java
@@ -19,3 +19,31 @@ public class StockController {
                 return stockService.effectuerSortieUnique(); 
             }
 }
+
+
+package episen.sirius.ing2.proto_back.controller;
+
+import episen.sirius.ing2.proto_back.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/stock")
+@CrossOrigin("*") // Permet au front de faire des requêtes vers le back
+public class StockController {
+
+    @Autowired
+    private StockService stockService;
+
+    @PostMapping("/sortie")
+    public String sortieStock() {
+        new Thread(() -> {
+            try {
+                stockService.effectuerSortiesNonStop();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        return "Sortie de stock en cours...";
+    }
+}
