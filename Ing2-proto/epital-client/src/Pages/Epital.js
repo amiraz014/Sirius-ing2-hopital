4323ab044d4584ec63d560c23a6bc99c8e5a1d70
Amiraz014 <amiradjaz@hotmail.com>
2025-03-03 05:59:29 +0100

    Main Page React 1

--- Ing2-proto/epital-client/src/Pages/Epital.js
+++ Ing2-proto/epital-client/src/Pages/Epital.js
@@ -1,5 +1,21 @@
 import {createContext} from 'react';
- export const Epital = createContext({
+import {useState} from 'react';
+
+const Epital = createContext({
     showMenu: false,
     setShowMenu: () => {},
  });
+
+ export const EpitalProvider = (props) => {
+     const [showMenu, setShowMenu] = useState(false);
+        return (
+            <Epital.Provider value={{
+                 showMenu: showMenu,
+                 setShowMenu : setShowMenu,
+                  }}>
+                {props.children}
+            </Epital.Provider>
+        );
+ };
+    export default Epital;
+ 
