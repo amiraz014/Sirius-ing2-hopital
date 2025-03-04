740a087286705a8eee701113c470e6e2746b326d
Amiraz014 <amiradjaz@hotmail.com>
2025-03-04 17:09:58 +0100

    changement in code

--- Ing2-proto/epital-server/src/main/java/episen/sirius/ing2/proto_back/controller/StockController.java
+++ Ing2-proto/epital-server/src/main/java/episen/sirius/ing2/proto_back/controller/StockController.java
@@ -1,49 +1,49 @@
-package episen.sirius.ing2.proto_back.controller;
+// package episen.sirius.ing2.proto_back.controller;
 
-import episen.sirius.ing2.proto_back.service.StockService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-
-@RestController
-@RequestMapping("/stock")
-@CrossOrigin(origins = "http://172.31.252.117:3000")
-public class StockController {
-    @Autowired
-    private StockService stockService;    
-    @GetMapping("/simulate/once")
-    public String simulateStockOnce() { 
-                return stockService.effectuerSortieUnique(); 
-            }
-}
+// import episen.sirius.ing2.proto_back.service.StockService;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-package episen.sirius.ing2.proto_back.controller;
+// @RestController
+// @RequestMapping("/stock")
+// @CrossOrigin(origins = "http://172.31.252.117:3000")
+// public class StockController {
+//     @Autowired
+//     private StockService stockService;    
+//     @GetMapping("/simulate/once")
+//     public String simulateStockOnce() { 
+//                 return stockService.effectuerSortieUnique(); 
+//             }
+// }
 
-import episen.sirius.ing2.proto_back.service.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/stock")
-@CrossOrigin("*") // Permet au front de faire des requêtes vers le back
-public class StockController {
+// package episen.sirius.ing2.proto_back.controller;
 
-    @Autowired
-    private StockService stockService;
+// import episen.sirius.ing2.proto_back.service.StockService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
 
-    @PostMapping("/sortie")
-    public String sortieStock() {
-        new Thread(() -> {
-            try {
-                stockService.effectuerSortiesNonStop();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
-        return "Sortie de stock en cours...";
-    }
-}
+// @RestController
+// @RequestMapping("/api/stock")
+// @CrossOrigin("*") // Permet au front de faire des requêtes vers le back
+// public class StockController {
+
+//     @Autowired
+//     private StockService stockService;
+
+//     @PostMapping("/sortie")
+//     public String sortieStock() {
+//         new Thread(() -> {
+//             try {
+//                 stockService.effectuerSortiesNonStop();
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }).start();
+//         return "Sortie de stock en cours...";
+//     }
+// }
