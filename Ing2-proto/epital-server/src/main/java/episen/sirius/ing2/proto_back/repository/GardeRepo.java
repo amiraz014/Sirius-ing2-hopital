61c36d66636743c60ca0394d7c5d04038c9aee50
Amiraz014 <amiradjaz@hotmail.com>
2025-03-21 04:40:13 +0100

    commit de rectification v3

--- Ing2-proto/epital-server/src/main/java/episen/sirius/ing2/proto_back/repository/GardeRepo.java
+++ Ing2-proto/epital-server/src/main/java/episen/sirius/ing2/proto_back/repository/GardeRepo.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,9 +16,12 @@ import episen.sirius.ing2.proto_back.model.Garde;
 @Repository
 public interface GardeRepo extends JpaRepository<Garde,Long> {
 
-    @Query("UPDATE Garde g SET g.employe.idE = :idE WHERE g.idG = :idG AND g.date = :date AND g.heure = :time")
-    Integer UpdateGarde(@Param("idE") Long idE,
-                        @Param("date") LocalDate dateAbsence,
-                        @Param("time") LocalTime timeAbsence,
-                        @Param("idG") Long idG);
+    @Modifying
+    @Query("UPDATE Garde g SET g.employe.idE = :idE WHERE g.date = :dateAbsence AND g.heure = :timeAbsence AND g.idG = :idG")
+    Integer UpdateGarde(
+            @Param("idE") Long idE,
+            @Param("dateAbsence") LocalDate date,
+            @Param("timeAbsence") LocalTime time,
+            @Param("idG") Long idG
+    );
 }
